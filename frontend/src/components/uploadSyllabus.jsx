b35a9d4f66cb0bfa82d7b2004e6487c4e4129ab7
Nathan Lee <nathan.l.lee123@gmail.com>
2020-02-29 14:18:02 -0800

    Updated frontend and server code

---
+++ frontend/src/components/uploadSyllabus.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default class UploadPage extends React.Component {
+  render() {
+    return (
+      <h1>Drop File here</h1>
+    )
+  }
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './App.css';
+
+//Components:
+import UploadPage from './components/uploadSyllabus'
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      documentUploaded : false,
+      documentBinary : '',
+
+      documentSubmitted : false,
+
+      calendarEvents : {},
+
+    };
+  }
+
+  onDocumentSubmit = (datatype) => {
+    if (datatype == "pdf") {
+
+    }
+
+    if (datatype == "doc") {
+
+    }
+
+    if (datatype == "image") {
+
+    }
+  }
+
+  onEventEdit = (eventID, editedEvent) {
+    this.state.calendarEvents[eventID] = editedEvent
+  }
+
+  render() {
+    if (this.state.documentUploaded === false) {
+      return (
+        <UploadPage />
+      );
+    } else {
+      return (
+        <
+      )
+    }
+
+  }
+
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
