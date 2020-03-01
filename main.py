9dfe39217bf1b9c4a24d1d98113e1abd144cffcc
collinstreun <swissflyer12@comcast.net>
2020-02-29 18:09:49 -0800

    Collin's additions

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from flask import Flask, escape, request, render_template
 
+from google.cloud import vision
+import io
+
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,11 +21,52 @@ def index():
 
 @app.route('/api/syllabus', methods=['POST'])
 def syllabus():
-    syllabusInput = request.get_json()
+    picture = request.body
+    detect_text(picture)
+
+def detect_text(path):
+    """Detects text in the file."""
+
+    client = vision.ImageAnnotatorClient()
+
+   # with io.open(path, 'rb') as image_file:
+   #     content = image_file.read() #content equals new parameter
+
+    content = path
+
+    image = vision.types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+    return "Worked"
+
+
+
+
+
 
 @app.route('/api/calendar', methods=['POST'])
 def calender():
-    calendInput = request.get_json()
+    calenderInput = request.get_json()
+
+
+
+def addEvent():
+    creds =
+
 
 if __name__ == '__main__':
     app.run(debug=True)
